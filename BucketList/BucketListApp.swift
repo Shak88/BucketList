d65b468f3bf3c03905bc10be1d062d992b0f2133
Shokri <shokre4@hotmail.com>
2022-07-04 23:01:38 +0400

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Shokri Alnajjar on 04/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
